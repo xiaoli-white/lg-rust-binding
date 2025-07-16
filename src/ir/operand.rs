e2a4d8c87f46218affd66fff04176aaa9d7e2899
XiaoLi <maxljh0216@yeah.net>
2025-07-16 20:37:50 +0800

    feat(ir): 添加中间表示（IR）模块的运算符定义
    - 新增了多种IR运算符结构体，包括虚拟寄存器、常量、宏、Phi节点、虚拟表、接口表等
    - 实现了Display trait以支持运算符的格式化输出
    - 实现了IRNode和IROperand trait以支持IR树的遍历和操作
    - 添加了简单的accept方法以支持未来IR遍历的扩展

--- src/ir/operand.rs
+++ src/ir/operand.rs
@@ -1,3 +1,186 @@
+use crate::ir::IRVisitor;
 use crate::ir::base::IRNode;
+use crate::ir::types::IRType;
+use std::fmt;
+use std::fmt::{Display, Formatter};
 
 pub trait IROperand: IRNode {}
+pub struct IRVirtualRegister {
+    pub name: String,
+}
+
+impl IRVirtualRegister {
+    fn new(name: String) -> Self {
+        Self { name }
+    }
+}
+
+impl Display for IRVirtualRegister {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "%{}", self.name)
+    }
+}
+
+impl IRNode for IRVirtualRegister {
+    fn accept(&self, visitor: &dyn IRVisitor) {
+        todo!()
+    }
+}
+
+impl IROperand for IRVirtualRegister {}
+
+pub struct IRConstant {
+    pub index: i32,
+}
+impl IRConstant {
+    fn new(index: i32) -> Self {
+        Self { index }
+    }
+}
+
+impl Display for IRConstant {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "${}", self.index)
+    }
+}
+impl IRNode for IRConstant {
+    fn accept(&self, visitor: &dyn IRVisitor) {
+        todo!()
+    }
+}
+impl IROperand for IRConstant {}
+
+pub struct IRMacro {
+    pub name: String,
+    pub args: Vec<String>,
+}
+
+impl IRMacro {
+    pub fn new(name: String, args: Vec<String>) -> Self {
+        Self { name, args }
+    }
+}
+impl Display for IRMacro {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "`{}({})", self.name, self.args.join(", "))
+    }
+}
+impl IRNode for IRMacro {
+    fn accept(&self, visitor: &dyn IRVisitor) {
+        todo!()
+    }
+}
+impl IROperand for IRMacro {}
+
+pub struct IRPhi {
+    pub _type: Box<dyn IRType>,
+    pub labels: Vec<String>,
+    pub operands: Vec<Box<dyn IROperand>>,
+}
+impl IRPhi {
+    pub fn new(
+        _type: Box<dyn IRType>,
+        labels: Vec<String>,
+        operands: Vec<Box<dyn IROperand>>,
+    ) -> Self {
+        Self {
+            _type,
+            labels,
+            operands,
+        }
+    }
+}
+impl Display for IRPhi {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        let s = format!("phi {} ", self._type.to_string())
+            + &self
+                .labels
+                .iter()
+                .zip(self.operands.iter())
+                .map(|(label, operand)| format!("[{}, {}]", label, operand))
+                .collect::<Vec<String>>()
+                .join(", ");
+        write!(f, "{}", s)
+    }
+}
+
+impl IRNode for IRPhi {
+    fn accept(&self, visitor: &dyn IRVisitor) {
+        todo!()
+    }
+}
+impl IROperand for IRPhi {}
+
+pub struct IRVirtualTable {
+    pub functions: Vec<String>,
+}
+
+impl IRVirtualTable {
+    pub fn new(functions: Vec<String>) -> Self {
+        Self { functions }
+    }
+}
+
+impl Display for IRVirtualTable {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(
+            f,
+            "IRVirtualTable{{functions={{{}}}}}",
+            self.functions.join(", ")
+        )
+    }
+}
+impl IRNode for IRVirtualTable {
+    fn accept(&self, visitor: &dyn IRVisitor) {
+        todo!()
+    }
+}
+impl IROperand for IRVirtualTable {}
+pub struct IRInterfaceTableEntry {
+    pub name: String,
+    pub functions: Vec<String>,
+}
+impl IRInterfaceTableEntry {
+    pub fn new(name: String, functions: Vec<String>) -> Self {
+        Self { name, functions }
+    }
+}
+impl Display for IRInterfaceTableEntry {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "Entry[name={}, functions={{{}}}]",
+            self.name,
+            self.functions.join(", ")
+        )
+    }
+}
+pub struct IRInterfaceTable {
+    pub entries: Vec<IRInterfaceTableEntry>,
+}
+
+impl IRInterfaceTable {
+    pub fn new(entries: Vec<IRInterfaceTableEntry>) -> Self {
+        Self { entries }
+    }
+}
+
+impl Display for IRInterfaceTable {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "IRInterfaceTable{{entries={{{}}}}}",
+            self.entries
+                .iter()
+                .map(|x| x.to_string())
+                .collect::<Vec<_>>()
+                .join(", ")
+        )
+    }
+}
+impl IRNode for IRInterfaceTable {
+    fn accept(&self, visitor: &dyn IRVisitor) {
+        todo!()
+    }
+}
+impl IROperand for IRInterfaceTable {}
