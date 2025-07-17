45099ea34d2c26806fdb744b5f9a84d246f41cd5
XiaoLi <maxljh0216@yeah.net>
2025-07-17 15:54:20 +0800

    feat(ir): 添加全局数据段和常量池结构
    - 新增 IRGlobalData、IRGlobalDataSection、IRBasicBlock、IRControlFlowGraph、IRFunction 等结构体
    - 实现相关结构体的 fmt::Display 和 IRNode trait
    - 添加 indexmap 依赖
    - 在 IRModule 中引入常量池 (IRConstantPool) 结构

--- src/ir.rs
+++ src/ir.rs
@@ -1,16 +1,60 @@
 use crate::ir::base::IRNode;
 use crate::ir::instruction::IRGoto;
+use crate::ir::types::IRType;
+use std::fmt::Display;
 
 mod base;
 mod instruction;
 mod operand;
 mod structure;
 mod types;
-
+pub struct IRConstantPoolEntry {
+    pub _type: Box<dyn IRType>,
+    pub value: Box<dyn Display>,
+}
+impl IRConstantPoolEntry {
+    pub fn new(_type: Box<dyn IRType>, value: Box<dyn Display>) -> Self {
+        IRConstantPoolEntry { _type, value }
+    }
+}
+impl Display for IRConstantPoolEntry {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Entry{{type={}, value={}}}", self._type, self.value)
+    }
+}
+impl IRNode for IRConstantPoolEntry {
+    fn accept(&self, visitor: &dyn IRVisitor) {
+        todo!()
+    }
+}
+pub struct IRConstantPool {
+    pub entries: Vec<IRConstantPoolEntry>,
+}
+impl Display for IRConstantPool {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let s = self
+            .entries
+            .iter()
+            .map(|entry| entry.to_string())
+            .collect::<Vec<_>>()
+            .join(", ");
+        write!(f, "IRConstantPool{{entries={{{}}}}}", s)
+    }
+}
+impl IRNode for IRConstantPool {
+    fn accept(&self, visitor: &dyn IRVisitor) {
+        for entry in &self.entries {
+            entry.accept(visitor);
+        }
+    }
+}
 pub struct IRModule {}
 
 pub trait IRVisitor {
-    fn visit(&self, ir_node: &dyn IRNode) where Self: Sized {
+    fn visit(&self, ir_node: &dyn IRNode)
+    where
+        Self: Sized,
+    {
         ir_node.accept(self)
     }
     fn visit_module(&self, ir_module: &IRModule);
--- src/ir/base.rs
+++ src/ir/base.rs
@@ -1,4 +1,10 @@
 use crate::ir::IRVisitor;
+use crate::ir::instruction::IRInstruction;
+use crate::ir::operand::IROperand;
+use crate::ir::structure::IRField;
+use crate::ir::types::IRType;
+use indexmap::IndexMap;
+use std::collections::{BTreeMap, HashMap};
 use std::fmt;
 
 pub trait IRNode: fmt::Display {
@@ -24,5 +30,173 @@ impl fmt::Display for IRCondition {
             IRCondition::GreaterEqual => "ge".to_string(),
         };
         write!(f, "{}", s)
+    }
+}
+
+pub struct IRGlobalData {
+    pub name: String,
+    pub size: Option<Box<dyn IROperand>>,
+    pub values: Option<Vec<Box<dyn IROperand>>>,
+}
+impl IRGlobalData {
+    pub fn new(
+        name: String,
+        size: Option<Box<dyn IROperand>>,
+        values: Option<Vec<Box<dyn IROperand>>>,
+    ) -> Self {
+        IRGlobalData { name, size, values }
+    }
+}
+impl fmt::Display for IRGlobalData {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.name)?;
+        if let Some(size) = &self.size {
+            write!(f, ", size={}", size)?;
+        }
+        if let Some(values) = &self.values {
+            write!(
+                f,
+                ", values=[{}]",
+                values
+                    .iter()
+                    .map(|v| v.to_string())
+                    .collect::<Vec<_>>()
+                    .join(", ")
+            )?;
+        }
+        Ok(())
+    }
+}
+impl IRNode for IRGlobalData {
+    fn accept(&self, visitor: &dyn IRVisitor) {
+        todo!()
+    }
+}
+pub struct IRGlobalDataSection {
+    pub data: Vec<IRGlobalData>,
+}
+impl fmt::Display for IRGlobalDataSection {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "IRGlobalDataSection{{data=[{}]}}",
+            self.data
+                .iter()
+                .map(|d| d.to_string())
+                .collect::<Vec<_>>()
+                .join(", ")
+        )
+    }
+}
+impl IRNode for IRGlobalDataSection {
+    fn accept(&self, visitor: &dyn IRVisitor) {
+        for data in &self.data {
+            data.accept(visitor);
+        }
+    }
+}
+pub struct IRBasicBlock {
+    pub name: String,
+    pub instructions: Vec<Box<dyn IRInstruction>>,
+}
+impl fmt::Display for IRBasicBlock {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "IRBasicBlock[name={}, instructions=[{}]]",
+            self.name,
+            self.instructions
+                .iter()
+                .map(|i| i.to_string())
+                .collect::<Vec<_>>()
+                .join(", ")
+        )
+    }
+}
+impl IRNode for IRBasicBlock {
+    fn accept(&self, visitor: &dyn IRVisitor) {
+        for instruction in &self.instructions {
+            instruction.accept(visitor);
+        }
+    }
+}
+pub struct IRControlFlowGraph {
+    pub basic_blocks: IndexMap<String, Box<IRBasicBlock>>,
+    pub out_edges: BTreeMap<Box<IRBasicBlock>, Vec<Box<IRBasicBlock>>>,
+    pub in_edges: BTreeMap<Box<IRBasicBlock>, Vec<Box<IRBasicBlock>>>,
+}
+impl IRControlFlowGraph {
+    pub fn new() -> Self {
+        let basic_blocks = IndexMap::new();
+        let out_edges = BTreeMap::new();
+        let in_edges = BTreeMap::new();
+        Self {
+            basic_blocks,
+            out_edges,
+            in_edges,
+        }
+    }
+    pub fn add_basic_block(&mut self, basic_block: Box<IRBasicBlock>) {
+        self.basic_blocks
+            .insert(basic_block.name.clone(), basic_block);
+    }
+}
+impl fmt::Display for IRControlFlowGraph {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "IRControlFlowGraph{{basicBlocks=[{}], outEdges=[], inEdges=[]}}",
+            self.basic_blocks
+                .values()
+                .map(|b| b.to_string())
+                .collect::<Vec<_>>()
+                .join(", ")
+        )
+    }
+}
+pub struct IRFunction {
+    pub return_type: Box<dyn IRType>,
+    pub name: String,
+    pub arguments_count: usize,
+    pub fields: Vec<Box<IRField>>,
+    pub control_flow_graph: Box<IRControlFlowGraph>,
+}
+impl IRFunction {
+    pub fn new(
+        return_type: Box<dyn IRType>,
+        name: String,
+        arguments_count: usize,
+        fields: Vec<Box<IRField>>,
+        control_flow_graph: Box<IRControlFlowGraph>,
+    ) -> Self {
+        IRFunction {
+            return_type,
+            name,
+            arguments_count,
+            fields,
+            control_flow_graph,
+        }
+    }
+}
+impl fmt::Display for IRFunction {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "IRFunction{{returnType={}, name='{}', argumentsCount={}, fields=[{}], controlFlowGraph={}}}",
+            self.return_type,
+            self.name,
+            self.arguments_count,
+            self.fields
+                .iter()
+                .map(|f| f.to_string())
+                .collect::<Vec<_>>()
+                .join(", "),
+            self.control_flow_graph
+        )
+    }
+}
+impl IRNode for IRFunction {
+    fn accept(&self, visitor: &dyn IRVisitor) {
+        todo!()
     }
 }
