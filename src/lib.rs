b59f6ca2467fa01ce850840e67c63fbbcf46e5b0
XiaoLi <maxljh0216@yeah.net>
2025-07-23 00:57:03 +0800

    feat: 添加 IR 生成器框架
    - 新增 IRGenerator 结构体和 generate 方法
    - 为后续的 IR 生成功能扩展打下基础

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
 extern crate core;
 
+use crate::ir::IRModule;
+
 pub mod ir;
+
+pub struct IRGenerator {}
+
+impl IRGenerator {
+    pub fn generate(ir_module: &IRModule, options: &Vec<String>) {
+        // TODO
+    }
+}
